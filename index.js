3411ac8267f3429e3cab3124c25d5b02fa4ae077
WtsCrackin <galomeggiolarobul@gmail.com>
2022-06-21 18:38:57 -0300

    reformuling img url

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const LATEST_TWEET__PLACEHOLDER = "%{{latest_tweet}}%";
     let readme = await fs.readFile('README.md.tpl', 'utf8');
     let feed = await parser.parseURL('https://rss.app/feeds/ic84UeZrXsfkpYbj.xml');
     let { creator, title, link, enclosure } = feed.items[0];
-    console.log(feed.items[0]);
+    const img = enclosure.url ? `<img src=${enclosure.url} alt="${creator}" style="max-width:90%; margin:2%" />
+    imf=enclosure.url? `: "";
     replaceable = `<div align="center">
     <a href=${link} style="text-decoration:none; padding:2% display:flex; flex-direction:column; justify-content:space-around; aling-items:center;">
     <div align="center" style="background-color:#0d0d0d; border-radius:16px; width:
     60%;">
     <p style="color:#fafafa; width:50%; font-size:1.2rem;">${title}</p>
-    <img src=${enclosure.url} alt="${creator}" style="max-width:90%; margin:2%" />
+    ${img}
     </div>
     </a>
     </div>`
