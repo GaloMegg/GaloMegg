0b7785a60c52d644bc379dc745d86edc321d5f68
Galo Meggiolaro <88691710+GaloMegg@users.noreply.github.com>
2022-06-26 18:02:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const LATEST_TWEET__PLACEHOLDER = "%{{latest_tweet}}%";
     let readme = await fs.readFile('README.md.tpl', 'utf8');
     let feed = await parser.parseURL('https://rss.app/feeds/ic84UeZrXsfkpYbj.xml');
     let { creator, title, link, enclosure } = feed.items[0];
-    const img = enclosure ? `<img src=${enclosure.url} alt="${creator}" style="max-width:90%; margin:2%" />
-    imf=enclosure.url? `: "<p></p>";
+    const img = enclosure ? `<img src=${enclosure.url} alt="${creator}" style="max-width:90%; margin:2%" />`: "<p></p>";
     replaceable = `<div align="center">
     <a href=${link} style="text-decoration:none; padding:2% display:flex; flex-direction:column; justify-content:space-around; aling-items:center;">
     <div align="center" style="background-color:#0d0d0d; border-radius:16px; width:
